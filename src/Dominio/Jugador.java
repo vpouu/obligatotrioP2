e8fef98a2f71636f598659567b655d1fdd0eb217
vpouu <victoriapou0@gmail.com>
2025-09-30 15:08:09 -0300

    ATRIBUTOS GETTERS Y SETTERS JUGADOR

--- src/Dominio/Jugador.java
+++ src/Dominio/Jugador.java
@@ -9,5 +9,20 @@ package Dominio;
  * @author victo
  */
 public class Jugador {
+    private String nombre;
+    private int edad;
+    public void setNombre( String elNombre){
+        this.nombre = elNombre;
+    
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setEdad(int laEdad){
+        this.edad = laEdad;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
     
 }
