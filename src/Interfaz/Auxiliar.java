a2a857d49eb07ac7a768d9ee3d107d76c1f4c40d
Martina <martimariagonzalez@gmail.com>
2025-09-30 14:34:07 -0300

    AHORA SI GUARDE

--- src/Interfaz/Auxiliar.java
+++ src/Interfaz/Auxiliar.java
@@ -1,8 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/* Victoria Pou y Martina Gonzalez*/
+
 package Interfaz;
+
+import java.util.Scanner;
 
 
 public class Auxiliar {
@@ -13,5 +13,24 @@ public class Auxiliar {
         System.out.println("=====================");
         System.out.println("");
     }
-    
+    public static int ingresarNumero(String texto, int min) {
+        return ingresarNumero(texto, min, Integer.MAX_VALUE);
+    }
+
+    public static int ingresarNumero(String texto, int min, int max) {
+        int ret = Integer.MIN_VALUE;
+        Scanner in = new Scanner(System.in);
+
+        while (ret < min || ret > max) {
+            System.out.print(texto);
+            ret = in.nextInt();
+            if (ret < min || ret > max) {
+                System.out.println("Ingreso es inválido.");
+            }
+        }
+
+        System.out.println("");
+        
+        return ret;
+    }
 }
