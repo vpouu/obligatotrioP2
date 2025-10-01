4e841ff99fa1b51e6d34432ee31d9a6c46e8562e
Martina <martimariagonzalez@gmail.com>
2025-10-01 16:56:21 -0300

    Update Tablero.java

--- src/Dominio/Tablero.java
+++ src/Dominio/Tablero.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Dominio;
 
-/**
- *
- * @author victo
- */
+
 public class Tablero {
     private String [][][][] matrizAMostrar = new String[4][7][3][2];
     private String [][] matrizLogica = new String[10][12];
