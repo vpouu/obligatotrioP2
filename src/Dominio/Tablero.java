08ca9cad52e29470beab13019d95dc8088d228a4
vpouu <victoriapou0@gmail.com>
2025-10-01 16:41:13 -0300

    creo clase tablero

---
+++ src/Dominio/Tablero.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dominio;
+
+/**
+ *
+ * @author victo
+ */
+public class Tablero {
+    private String [][][][] matrizAMostrar = new String[4][7][3][2];
+    private String [][] matrizLogica = new String[10][12];
+    public agregarMovimiento(){
+    }
+    public boolean terminarPartida(){
+    }
+    public int quienGano(){
+    
+    }
+    
+}
