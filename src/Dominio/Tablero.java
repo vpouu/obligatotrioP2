a481963a6d31c8086dd9882523c297eed1a6210d
vpouu <victoriapou0@gmail.com>
2025-10-01 16:56:35 -0300

    tablero

--- src/Dominio/Tablero.java
+++ src/Dominio/Tablero.java
@@ -9,14 +9,32 @@ package Dominio;
  * @author victo
  */
 public class Tablero {
-    private String [][][][] matrizAMostrar = new String[4][7][3][2];
-    private String [][] matrizLogica = new String[10][12];
-    public agregarMovimiento(){
+    private String [][][][] matrizAMostrar;
+    private String [][] matrizLogica;
+    private int turno;
+    //el blanco es 1, el negro es 2
+    public Tablero(){
+        this.turno = 1;
+        this.matrizAMostrar = new String[4][7][3][2];
+        this.matrizLogica = new String[3][6];
+        
+    }
+    /*
+    public void analizarEntrada(){}
+    public String agregarMovimiento(String movimiento){
     }
     public boolean terminarPartida(){
     }
     public int quienGano(){
     
     }
+    public String jugadaGanadora(){
+    
+    }
+    public String tableroAMostrar(){
+    }
+    public void ocultarFilas(){
+    }
+*/
     
 }
