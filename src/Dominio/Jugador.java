33e97ee86be3e04d783544923da3013a59d24085
Martina <martimariagonzalez@gmail.com>
2025-10-03 10:23:46 -0300

    fuciona?

--- src/Dominio/Jugador.java
+++ src/Dominio/Jugador.java
@@ -28,6 +28,7 @@ public class Jugador implements Comparable<Jugador>{
     }
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     public int getCantJugadas(){
         return this.cantGanadas;
     }
@@ -56,6 +57,9 @@ public class Jugador implements Comparable<Jugador>{
     public Jugador(String elNombre, int laEdad){
         this.cantGanadas = 0;
         this.cantJugadas = 0;
+=======
+    public Jugador(String elNombre, int laEdad){
+>>>>>>> Stashed changes
 =======
     public Jugador(String elNombre, int laEdad){
 >>>>>>> Stashed changes
