168ae74f80a30fcf168e12a25221f80383e410f4
vpouu <victoriapou0@gmail.com>
2025-10-10 14:08:27 -0300

    subo

--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -59,11 +59,17 @@ public class Interfaz {
         }
         //elegir jugador a participar, new partida, 
     }
+    public void mostrarTurno(Partida partida){
+        String ret ="Es el turno del jugador "+ partida.jugadorBlaONeg() + " ( " + partida.getJugadorActual();
+        System.out.println(ret);
+    }
     public void jugando(Partida partida){
         boolean termino=false;
         while(!termino){
             String [][] matLogica=partida.getTablero().getMatrizLogica();
+            mostrarTurno(partida);
             mostrarMatrizLogica(matLogica);
+            termino =true;
         }
     }
     public void mostrarMatrizLogica (String [][] matLogica){
